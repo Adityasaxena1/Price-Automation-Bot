51d8007f840fe4a6c2e4d95de20cbde49943943d
Aditya Saxena <144054604+Adityasaxena1@users.noreply.github.com>
2023-10-29 22:32:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from login import SellerLogin
 from search import SecondTab
-from popup_notification import MessageBox
+from popup_notification import pop_ups
 import math
 import pandas as pd
 
@@ -11,7 +11,7 @@ product_bot = SecondTab()
 
 high_price_model_list = []
 
-file_path = "model_id2.xlsx"
+file_path = "model_id.xlsx"
 df = pd.read_excel(file_path)
 
 my_list = df["Model CODE"].tolist()
@@ -23,30 +23,23 @@ for x in my_list:
 for value in model_ids:
     product_bot.product_search(value)
     if not product_bot.selected:
-        for price in [min(product_bot.price_list, key=int)]:
-            other_company_price = price
-            if int(product_bot.our_price) > int(other_company_price):
-                price_difference = int(product_bot.our_price) - int(other_company_price)
-                if price_difference > 20:
-                    high_price_model_list.append(value)
-                else:
-                    our_account_bot.update_price(price=int(other_company_price), model_id=value)
-            elif int(product_bot.our_price) == int(other_company_price):
-                our_account_bot.update_price(price=int(other_company_price), model_id=value)
+        other_company_price = int(product_bot.offer_price)
+        price_difference = abs(int(product_bot.our_price) - other_company_price)
+        if price_difference > 20:
+            high_price_model_list.append(value)
+        else:
+            our_account_bot.update_price(price=other_company_price, model_id=value)
 
 if len(high_price_model_list) > 0:
     for mod_id in high_price_model_list:
         product_bot.product_search(mod_id)
-        for prices in [min(product_bot.price_list, key=int)]:
-            other_price = prices
-            difference = int(product_bot.our_price) - int(other_price)
-            box = MessageBox()
-            user_choice = box.pop_ups(mod_id=mod_id, difference=difference, other_price=other_price,
-                                      ours=product_bot.our_price)
-            if user_choice:
-                our_account_bot.update_price(price=int(other_price), model_id=mod_id)
-            else:
-                pass
+        other_price = product_bot.offer_price
+        difference = int(product_bot.our_price) - int(other_price)
+        user_choice = pop_ups(mod_id=mod_id, difference=difference, other_price=other_price,
+                              ours=product_bot.our_price)
+        if user_choice:
+            our_account_bot.update_price(price=int(other_price), model_id=mod_id)
 
 our_account_bot.driver1.quit()
 product_bot.driver2.quit()
+
