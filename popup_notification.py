479874a5ad228e0118d78aab75b910e1d143b020
Aditya Saxena <144054604+Adityasaxena1@users.noreply.github.com>
2023-10-29 22:34:04 +0530

    Update popup_notification.py

--- popup_notification.py
+++ popup_notification.py
@@ -1,15 +1,21 @@
-from tkinter import messagebox
+from PyQt5.QtWidgets import QApplication, QMessageBox
+
+app = QApplication([])
 
 
-class MessageBox:
-    def __init__(self):
-        pass
+def pop_ups(mod_id, difference, ours, other_price):
+    msg_box = QMessageBox()
+    msg_box.setIcon(QMessageBox.Question)
+    msg_box.setWindowTitle("Price Alert!")
+    msg_box.setText(f"Price difference for Model ID {mod_id} is ₹{difference}\n"
+                    f"Our Price: ₹{ours}\n"
+                    f"Offer Price: ₹{other_price}\n"
+                    f"Do you want to make our price equal to Offer Price? \n"
+                    f"Choose 'yes' or 'no' ")
+    msg_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+    result = msg_box.exec_()
 
-    def pop_ups(self, mod_id, difference, ours, other_price):
-        choice = messagebox.askyesno(title="Price Alert!",
-                                     message=f"Price difference for Model ID {mod_id} is ₹{difference}\n"
-                                             f"Our Price: ₹{ours}\n"
-                                             f"Other's Price: ₹{other_price}\n"
-                                             f"Do you want to make our price decrease by ₹1 than other's? \n"
-                                             f" Choose 'yes' or 'no' ")
-        return choice
+    if result == QMessageBox.Yes:
+        return True
+    else:
+        return False
